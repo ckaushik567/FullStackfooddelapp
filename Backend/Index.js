9f21f42c29d919be000ccd40ccfe240d4933f420
Chanchal kaushik <ckaushik593@gmail.com>
2024-12-04 12:50:07 +0530

    update cors

--- Backend/Index.js
+++ Backend/Index.js
@@ -20,6 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
+const corsOptions = {
+    origin: "http://localhost:5000",
+    credentials: true,
+  };
+
 app.use('/', users);
 app.use('/',foodItem);
 app.use('/', profile);
