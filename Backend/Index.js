05933b6f68917df702f85fe7df0ea12c07ab419e
Chanchal kaushik <123247218+ckaushik567@users.noreply.github.com>
2024-12-04 13:33:51 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const corsOptions = {
-    origin: "http://localhost:5000",
+    origin: "https://fullstackfooddelapp-9.onrender.com/",
     credentials: true,
   };
 
